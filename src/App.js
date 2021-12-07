3856d4903a4dafae475eb9b6c01e077641c0dca6
MauricioMiramontes <mauriciomiramontes58@aragon.unam.mx>
2021-12-07 15:46:31 -0600

    Se eliminan archivos inecesarios

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ import "assets/css/nucleo-icons.css";
 import "assets/scss/blk-design-system-react.scss?v=1.2.0";
 import "assets/demo/demo.css";
 
-import Index from "views/Index.js";
 import Inicio from "views/Inicio.js";
 import Contacto from "views/Contacto";
 import LineaTiempo from "views/LineaTiempo";
-import LandingPage from "views/examples/LandingPage.js";
-import RegisterPage from "views/examples/RegisterPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
 
 function App() {
 
@@ -21,19 +17,6 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/components" render={(props) => <Index {...props} />} />
-        <Route
-          path="/landing-page"
-          render={(props) => <LandingPage {...props} />}
-        />
-        <Route
-          path="/register-page"
-          render={(props) => <RegisterPage {...props} />}
-        />
-        <Route
-          path="/profile-page"
-          render={(props) => <ProfilePage {...props} />}
-        />
         <Route path="/inicio" render={(props) => <Inicio {...props} lenguage={lenguage} changeLenguage={(e) => setLenguage(e)} />} />
         <Route path="/contacto" render={(props) => <Contacto {...props} lenguage={lenguage} changeLenguage={(e) => setLenguage(e)} />} />
         <Route path="/linea-tiempo" render={(props) => <LineaTiempo {...props} lenguage={lenguage} changeLenguage={(e) => setLenguage(e)} />} />
